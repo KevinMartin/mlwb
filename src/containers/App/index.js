144f27dd38391cc0cbfc544d53dc24252e4ddf30
Kevin Martin <kevin@tiliq.com>
2017-01-30 19:20:00 -0500

    refactor(App.js): Make App.js a pure function since it doesn't use state

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,34 +1,26 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Header, Sidebar } from 'components';
 import styles from './styles.scss';
 
-export default class App extends Component {
-	static propTypes = {
-		children: PropTypes.object.isRequired
-	}
+const App = ({ children }) => (
+	<div className={styles.container}>
+		<div className="container">
+			<Header />
 
-	static contextTypes = {
-		store: PropTypes.object.isRequired
-	}
-
-	constructor(props, context) {
-		super(props, context);
-
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<div className={styles.container}>
-				<div className="container">
-					<Header />
-
-					<div className={`row ${styles.content}`}>
-						<div className="col-xs-12 col-md-9">{this.props.children}</div>
-						<Sidebar className="col-xs-12 col-md-3" />
-					</div>
-				</div>
+			<div className={`row ${styles.content}`}>
+				<div className="col-xs-12 col-md-9">{children}</div>
+				<Sidebar className="col-xs-12 col-md-3" />
 			</div>
-		);
-	}
-}
+		</div>
+	</div>
+);
+
+App.propTypes = {
+	children: PropTypes.object.isRequired
+};
+
+App.contextTypes = {
+	store: PropTypes.object.isRequired
+};
+
+export default App;
